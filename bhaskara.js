4c46df2b42cbc975e34b465bb369a8dda8b71b60
CharlesRLopes <93800577+CharlesRLopes@users.noreply.github.com>
2022-08-27 20:32:27 -0300

    Add files via upload

---
+++ bhaskara.js
@@ -0,0 +1,48 @@
+/*Questão 02 - Bhaskara
+Uma das vantagens da programação é a automatização de tarefas que não gostamos de realizar. Dito isto, 
+elabore um método em JS cujo objetivo é resolver a fórmula de Bhaskara. 
+Para isso, sua função deve receber três parâmetros, “ax2”, “bx” e “c”, de tal modo que na equação: 3x² - 5x + 12 
+os valores seriam respectivamente: 3, -5, 12. 
+Como retorno deve ser passado um vetor que tem 2 valores um para cada possível resultado, 
+mesmo que os resultados sejam iguais.
+Caso o delta seja negativo, retorne, ao invés do vetor, um string com a frase: “Delta é negativo”. */
+
+const valor_a = document.querySelector("#valor_a");
+const valor_b = document.querySelector("#valor_b");
+const valor_c = document.querySelector("#valor_c");
+const res = document.querySelector("#resultado");
+const botao = document.querySelector("#calcular");
+const aviso = document.querySelector("#aviso");
+
+const bhaskara = (a, b, c) => {
+    a = Number.parseInt(a);
+    b = Number.parseInt(b);
+    c = Number.parseInt(c);
+    x = []
+    delta = b * b -4 * a * c;
+
+    if(delta < 0){
+        return "Delta é negativo";  
+    } 
+    else{
+        x[0] = ((-b + Math.sqrt(delta)) / (2 * a));
+        x[1] = ((-b - Math.sqrt(delta)) / (2 * a));
+        return x;
+    }
+}
+
+botao.onclick = () => {
+    if (valor_a.value == "" || valor_b.value == "" || valor_c.value == ""){
+        aviso.innerText = "Necessário inserir os valores de a, b, c.";
+    }
+    else if (valor_a.value !== '0'){
+        res.value= bhaskara(valor_a.value, valor_b.value, valor_c.value);
+        aviso.innerText = "";
+    }
+    else{
+        aviso.innerText = "Valor de 'a' deve ser diferente de 0";
+    }
+}
+
+
+
---
+++ sistemaDeNotas.js
@@ -0,0 +1,44 @@
+/* Questão 03 - Sistema de Notas
+Construa um código JS que receba uma valor através de um input para um sistema de notas de uma 
+instituição que possui a seguinte política de classificação: 
+* Todo aluno recebe uma nota de 0 a 100. 
+* Alunos com nota abaixo de 40 são reprovados. 
+* As notas possuem a seguinte regra de arredondamento: 
+	* Se a diferença entre a nota e o próximo múltiplo de 5 for menor que 3, 
+    arredondar a nota para esse próximo múltiplo de 5. 
+	* Se a nota for abaixo de 38, não é feito nenhum arredondamento pois esta nota resulta na reprovação do aluno. 
+Por exemplo, a nota 84 será arredondada para 85, mas a nota 29 não será arredondada por ser abaixo de 40 
+e não ser possível arredondamento eficiente, ou seja, que evite a reprovação do aluno. 
+No caso de a nota ser 38, o arredondamento é possível pois atingirá 40 e o aluno será aprovado. */
+
+// Selecionado os elementos da tela.
+const nota = document.querySelector("#nota");
+const res = document.querySelector("#resultado");
+const botao = document.querySelector("#calcular");
+const aviso = document.querySelector("#aviso");
+
+const resultado = (valor) => {
+    valor = Number.parseFloat(valor);
+    if (valor >= 0 && valor <= 100){
+        if (valor % 5 >= 3 && valor >= 38){
+            valor += 5 - valor % 5; 
+        }
+        if (valor >= 40){
+            aviso.innerText = "Aprovado";
+            return valor;
+        }
+        else{
+            aviso.innerText = "Reprovado";
+            return valor;
+        }
+    }
+}
+botao.onclick = () => {
+    if (nota.value == "" || nota.value < 0 || nota.value > 100){
+        aviso.innerText = "Necessário Campo de Nota com Valor entre 0 e 100";
+        res.value = "";
+    }
+    else{
+        res.value = resultado(nota.value);
+    }
+}
